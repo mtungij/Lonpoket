ea81c48ed83fd9ca14f5fb24f1869d9e4c3b104b
mtungij <jamesmtungi2013@gmail.com>
2025-06-06 17:43:01 +0300

    first

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -161,7 +161,8 @@ include_once APPPATH . "views/partials/header.php";
           <td class="py-2">KILA SIKU</td>
           <td class="text-right">
             <span class="inline-block bg-green-600 text-white text-xs px-2 py-1 rounded">
-              <?php echo number_format($total_withdrawal_daily->loan_aproved ?? 0); ?>
+              <!-- </?php echo number_format($total_withdrawal_daily->loan_aproved ?? 0); ?> -->
+               coming Soon
             </span>
           </td>
         </tr>
@@ -169,7 +170,8 @@ include_once APPPATH . "views/partials/header.php";
           <td class="py-2">WIKI</td>
           <td class="text-right">
             <span class="inline-block bg-green-600 text-white text-xs px-2 py-1 rounded">
-              <?php echo number_format($total_withdrawal_weekly->loan_aproved ?? 0); ?>
+              <!-- </?php echo number_format($total_withdrawal_weekly->loan_aproved ?? 0); ?> -->
+              coming Soon
             </span>
           </td>
         </tr>
